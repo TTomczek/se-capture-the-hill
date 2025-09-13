b0db4ab4756d80abd1d6d866c3c5a1dcfebccfb6
ttomczek <>
2025-09-13 12:54:57 +0200

    Changed format of HelpCommand Output

--- Content/Data/Scripts/Capture the Hill/commands/HelpCommand.cs
+++ Content/Data/Scripts/Capture the Hill/commands/HelpCommand.cs
@@ -5,7 +5,6 @@ namespace CaptureTheHill.Content.Data.Scripts.Capture_the_Hill.commands
 {
     public static class HelpCommand
     {
-        
         public static bool IsCommandResponsible(string messageText)
         {
             return messageText.Equals("help") || messageText.Equals("?");
@@ -13,10 +12,13 @@ namespace CaptureTheHill.Content.Data.Scripts.Capture_the_Hill.commands
 
         public static void Execute(List<IChatCommand> chatCommands)
         {
+            var chatString = "\nAvailable commands:\n";
             foreach (var command in chatCommands)
             {
-                MyAPIGateway.Utilities.ShowMessage("CTH", command.GetHelp());
+                chatString += $"{command.GetHelp()}\n";
             }
+
+            MyAPIGateway.Utilities.ShowMessage("CTH", chatString);
         }
     }
 }
